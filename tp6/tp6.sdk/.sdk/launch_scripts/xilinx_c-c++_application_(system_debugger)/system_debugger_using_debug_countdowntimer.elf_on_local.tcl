cb92757d4ed159f42cc1ae26719c1f04f5809ea1
Ricardo Filipe <ricardo.filipe_95@hotmail.com>
2018-05-02 16:54:44 +0100

    commit to presentation

---
+++ tp6/tp6.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_using_debug_countdowntimer.elf_on_local.tcl
@@ -0,0 +1,7 @@
+connect -url tcp:127.0.0.1:3121
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Nexys4 210274674840A"} -index 0
+rst -processor
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Nexys4 210274674840A"} -index 0
+dow D:/Universidade/cr/tp6/tp6.sdk/CountDownTimer/Debug/CountDownTimer.elf
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Nexys4 210274674840A"} -index 0
+con
